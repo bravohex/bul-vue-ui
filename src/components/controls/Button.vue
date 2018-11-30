6c0aad35e1e50e363c0f56f8661f6f34f826dab1
bravohex <lmt.register@outlook.com>
2018-11-30 23:51:29 +0700

    working on  button component

---
+++ src/components/controls/Button.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Button",
+  props: {
+    text: String,
+    disabled: Boolean
+  }
+});
+</script>
+
+<style lang="sass" scoped>
+
+</style>
